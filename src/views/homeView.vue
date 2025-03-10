bd99e743782db37b8120efe468751ecb97bdb2f6
Christian Johnson <trajon2011@gmail.com>
2025-03-10 17:00:44 -0500

    5pm push

--- src/views/homeView.vue
+++ src/views/homeView.vue
@@ -1,38 +1,11 @@
 <script>
-/*import FooterComponent from '../components/footerComponent.vue';*/
-
 export default {
   name: 'HomeView',
-  /*components: {
-    FooterComponent
-  },*/
 }
 </script>
 
 <template>
-  <div class="contentpage">
-      <!--<img src="../assets/dummy4.jpg" class="showcase" alt="showcase1">
-      <img src="../assets/dummy5.jpg" class="showcase" alt="showcase1">
-      <img src="../assets/dummy6.jpg" class="showcase" alt="showcase1">-->
 
-      <div class="hubbox">
-      <div class="hubpiece"></div>
-      <div class="hubpiece2">
-        <h1>QUXK</h1>
-      </div>
-      <div class="hubpiece3"></div>
-      <div class="hubpiece4"></div>
-      <div class="hubpiece5"></div>
-      <div class="hubpiece6"></div>
-      <div class="hubpiece7"></div>
-      <div class="hubpiece8"></div>
-      </div>
-
-
-  </div>
-  <footer>
-    <FooterComponent />
-  </footer>
 </template>
 
 <style>
@@ -63,131 +36,10 @@ h1 {
   align-items: center;
 }
 .contentpage {
-  width: 100%;             /* Takes full width of the parent container */
-  height: 70vh;           /* Full viewport height */
-  display: flex;           /* Enable flexbox */
-  justify-content: center; /* Center .hubbox horizontally */
-  align-items: center;     /* Center .hubbox vertically */
+  width: 100%;  
+  height: 70vh;          
+  display: flex;          
+  justify-content: center; 
+  align-items: center;    
 }
-.hubbox {
-  width: 1000px;           /* Fixed width */
-  height: 900px;           /* Fixed height */
-  background-color: #354464;
-  border-style: ridge;
-  border-width: 5px;
-  border-color: rgb(80, 138, 255);
-  border-radius: 7px;
-  position: relative;
-  outline: 4px solid black;
-}
-.hubpiece {
-  width: 150px;             
-  height: 530px;            
-  background-color: #354464;       
-  border-style: ridge;
-  border-width: 5px;
-  border-color: rgb(80, 138, 255);
-  border-radius: 7px 7px 0px 70px;
-  position: absolute;
-  top: 20%;
-  left: 2%;
-  outline: 4px solid black;
-
-}
-.hubpiece2 {
-  width: 950px;             
-  height: 150px;            
-  background-color: #354464;     
-  border-style: ridge;
-  border-width: 5px;
-  border-color: rgb(80, 138, 255);
-  border-radius: 7px 7px 70px 70px;
-  position: absolute;
-  top: 2%;
-  left: 2%;
-  outline: 4px solid black;
-}
-.hubpiece3 {
-  width: 621px;             
-  height: 250px;            
-  background-color: #354464;     
-  border-style: ridge;
-  border-width: 5px;
-  border-color: rgb(80, 138, 255);
-  border-radius: 7px 7px 7px 7px;
-  position: absolute;
-  top: 20.5%;
-  left: 183px;
-  outline: 4px solid black;
-}
-.hubpiece4 {
-  width: 950px;             
-  height: 150px;            
-  background-color: #354464;     
-  border-style: ridge;
-  border-width: 5px;
-  border-color: rgb(80, 138, 255);
-  border-radius: 70px 70px 7px 7px;
-  position: absolute;
-  top: 81%;
-  left: 2%;
-  outline: 4px solid black;
-}
-.hubpiece5 {
-  width: 150px;             
-  height: 530px;            
-  background-color: #354464;       
-  border-style: ridge;
-  border-width: 5px;
-  border-color: rgb(80, 138, 255);
-  border-radius: 7px 7px 70px 0px;
-  position: absolute;
-  top: 20%;
-  left: 82.5%;
-  outline: 4px solid black;
-
-}
-.hubpiece6 {
-  width: 621px;             
-  height: 80px;            
-  background-color: #354464;     
-  border-style: ridge;
-  border-width: 5px;
-  border-color: rgb(80, 138, 255);
-  border-radius: 7px 7px 7px 7px;
-  position: absolute;
-  top: 50%;
-  left: 183px;
-  outline: 4px solid black;
-}
-.hubpiece7 {
-  width: 310.5px;             
-  height: 170px;            
-  background-color: #354464;     
-  border-style: ridge;
-  border-width: 5px;
-  border-color: rgb(80, 138, 255);
-  border-radius: 7px 7px 7px 7px;
-  position: absolute;
-  top: 60.25%;
-  left: 183px;
-  outline: 4px solid black;
-}
-.hubpiece8 {
-  width: 305px;             
-  height: 170px;            
-  background-color: #354464;     
-  border-style: ridge;
-  border-width: 5px;
-  border-color: rgb(80, 138, 255);
-  border-radius: 7px 7px 7px 7px;
-  position: absolute;
-  top: 60.25%;
-  left: 500px;
-  outline: 4px solid black;
-}
-/*.showcase {
-  width: 400px;
-  height: 400px;
-}*/
 </style>
---
+++ src/views/oldnavbar.vue
@@ -0,0 +1,118 @@
+<template>
+<div>
+    <!-- Bootstrap based Nav bar -->
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+      <div class="container d-flex align-items-center">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/about">about</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/contact">contact</router-link>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdownMenuLink"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false">
+
+              portfolio
+
+            </a>
+
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <li class="nav-item">
+                <router-link class="nav-link" to="/2D">2D</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/3D">3D</router-link>
+              </li>
+            </ul>
+          </li>
+
+          
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdownMenuLink"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false">
+
+              commissions
+
+            </a>
+
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <li class="nav-item">
+                <router-link class="nav-link" to="/2Dprices">prices - 2D</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/3Dprices">prices - 3D</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/T.O.S">T.O.S</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/form">form</router-link>
+              </li>
+            </ul>
+          </li>
+        </ul>
+
+        
+        <a class="navbar-brand" href="#">
+          <img
+            alt="Rhurbo Logo"
+            src="./assets/rhurbo_logo_b.png"
+            width="48"
+            height="48"
+            class="d-inline-block align-text-top"
+          />
+        </a>
+
+        <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="https://www.instagram.com/trajon.j/">
+            <img src="./assets/instagram.png" alt="Instagram" width="20" height="20" />
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://www.tiktok.com/@trajonj">
+            <img src="./assets/tik-tok.png" alt="tiktok" width="20" height="20" />
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://twitter.com/trajonj">
+            <img src="./assets/twitter.png" alt="twitter" width="20" height="20" />
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://www.youtube.com/channel/UCDd0g1jvbjNzlEDXATTdHpQ">
+            <img src="./assets/youtube.png" alt="twitter" width="20" height="20" />
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="trajon2011@gmail.com">
+            <img src="./assets/email.png" alt="email" width="20" height="20" />
+          </a>
+        </li>
+      </ul>
+
+      </div>
+    </nav>
+
+    <div class="container mt-4">
+      <!-- Router view - our main view below the navigation -->
+      <RouterView></RouterView>
+    </div>
+</div>
+</template>
