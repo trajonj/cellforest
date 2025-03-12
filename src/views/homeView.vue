170879b6c65d7a28924ef273dd372440282f9202
Christian Johnson <140658351+trajonj@users.noreply.github.com>
2025-03-12 10:54:59 -0500

    logo

--- src/views/homeView.vue
+++ src/views/homeView.vue
@@ -5,12 +5,18 @@ export default {
 </script>
 
 <template>
-
+  <div class="container">
+    <h1>HOME</h1>
+    <p>Page content goes here</p>
+    <img src="@/assets/images/dummy4.jpg" alt="showcase1">
+  </div>
 </template>
 
 <style>
-html, body {
+html, body, #app {
   background-color: #1b1b1b;
+  margin: 0;
+  height: 100%;
 }
 
 body {
@@ -18,7 +24,7 @@ body {
     font-size: 14px;
     /* Replace #333333 with your desired font color */
   }
-  @font-face {
+@font-face {
   font-family: "ledlight";
   src: url("@/assets/fonts/ledlight/LEDLIGHT.otf") format("OpenType");
   font-weight: normal;
@@ -28,18 +34,15 @@ h1 {
   font-family: "ledlight", sans-serif;
   color: #d4d4d4de;
   font-size: 72px;
+  text-align: center;
 }
 .container {
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  width: 1088px !important;
+  height: 1163px !important;
+  padding: 70px;
+  margin: 90px auto; /* Centers horizontally */
+  border: 5px solid rgb(91, 154, 255); /* Optional */
+  box-sizing: border-box;
 }
-.contentpage {
-  width: 100%;  
-  height: 70vh;          
-  display: flex;          
-  justify-content: center; 
-  align-items: center;    
-}
+
 </style>
