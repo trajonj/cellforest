48c244f92af2fc60aa11331fa8d308c6eeac673b
Christian Johnson <140658351+trajonj@users.noreply.github.com>
2025-03-12 13:09:32 -0500

    1 pm push

--- src/views/homeView.vue
+++ src/views/homeView.vue
@@ -8,6 +8,7 @@ export default {
 <div class="contentpage">
   <div class="hubbox">
     <div class="titlebar"><h1>RHURBO</h1></div>
+    <div class="navbuttons"><button class="glass-btn">Glassy</button></div>
   </div>
 </div>
 </template>
@@ -65,5 +66,92 @@ h1 {
   justify-content: center; /* Center .hubbox horizontally */
   align-items: center;     /* Center .hubbox vertically */
 }
-
+.navbuttons{
+  
+}
+.glass-btn {
+  user-select: none;
+  font-size: clamp(1.2rem, 5vw + 1rem, 2.5rem);
+  width: 10em;
+  height: 4em;
+  
+  border-radius: 0.5em;
+  background-image: linear-gradient(#0003,#0000);
+  
+  box-shadow:
+		0 -0.125em 0.25em #0002,
+		0 0.25em 0.25em hsl(var(--h) var(--s) var(--l) / 0.5),
+		0 0 0 0.1em hsl(var(--h) var(--s) var(--l) / 0.5),
+		0 0.175em 0.3em hsl(var(--h) var(--s) var(--l) / 0.5) inset,
+		0 -0.025em 0.175em hsl(var(--h) var(--s) var(--l) / 0.4) inset,
+		0 -0.25em 1em 0.3em hsl(var(--h) var(--s) var(--l) / 0.3) inset,
+		0 0.6em 0 hsl(var(--h) var(--s) var(--l) / 0.3) inset,
+		0 2em 1em #0004;
+  backdrop-filter: blur(0.15em);
+  position: relative;
+  display: grid;
+  place-content: center;
+  color: hsl(var(--h) var(--s) var(--l) / .7);
+  text-shadow: 
+    0.03em 0.03em #fff5,
+    -0.03em -0.03em #0005;
+  cursor: pointer;
+  transition: 0.1s ease;
+  padding-top: 0.2em;
+}
+.glass-btn:before {
+  content: '';
+	position: absolute;
+  top: 100%;
+  width: 80%;
+  left: 10%;
+  height: 1.5em;
+  
+  background-image: 
+    radial-gradient(
+      100% 100% at center,
+      hsla(var(--h),var(--s),80%,0.25),
+      hsla(var(--h),var(--s),80%,0) 50%);
+}
+.glass-btn:after {
+  content: '';
+  inset: 0;
+  top: 0.5em;
+  position: absolute;
+  
+  background-image: 
+    linear-gradient(
+      105deg,
+      transparent 20%,
+      hsl(var(--h) var(--s) var(--l) / .2) 20%,
+      hsl(var(--h) var(--s) var(--l) / .2) 30%,
+      transparent 30%,
+      transparent 32%,
+      hsl(var(--h) var(--s) var(--l) / .2) 5%,
+      hsl(var(--h) var(--s) var(--l) / .2) 40%,
+      transparent 0%
+    );
+  background-size: 400% 100%;
+  background-position: 100% 0%;
+  transition: .3s ease;
+}
+.glass-btn:active:after {
+  background-position: -50% 0%;
+}
+.glass-btn:active {
+  translate: .01em .25em;
+  box-shadow:
+		0 -0.125em 0.25em #0002,
+		0 0.25em 0.25em hsl(var(--h) var(--s) var(--l) / 0.5),
+		0 0 0 0.1em hsl(var(--h) var(--s) var(--l) / 0.5),
+		0 0.175em 0.3em hsl(var(--h) var(--s) var(--l) / 0.8) inset,
+		0 -0.025em 0.175em hsl(var(--h) var(--s) var(--l) / 0.4) inset,
+		0 -0.25em 1em 0.3em hsl(var(--h) var(--s) var(--l) / 0.3) inset,
+		0 0.6em 0 hsl(var(--h) var(--s) var(--l) / 0.3) inset,
+		0 1em 0.5em #0004;
+  backdrop-filter: blur(0.08em);
+}
+.glass-btn:active:before {
+  height: 1em;
+}
 </style>
